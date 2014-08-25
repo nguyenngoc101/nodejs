447ab9fb206d446c82a35b5a1b434d2f978b01de
ngocnv <nguyenngoc101@yahoo.com>
2014-08-25 18:04:34 +0700

    add html files

--- chatServer/server.js
+++ chatServer/server.js
@@ -6,6 +6,10 @@ var url     = require('url');
 
 var root = __dirname;
 var INDEX_PATH = "public/index.html";
+var TEMPLATE_PATH = "public/template.html"
+var STYLE_PATH = "public/stylesheets/style.css"
+var cssFile = getTemplate(path.join(root,STYLE_PATH));
+var htmlTemplate = getTemplate(path.join(root,TEMPLATE_PATH)).replace('%style', cssFile);
 
 var server = http.createServer(function(req, res) {
   var ur = url.parse(req.url);
@@ -15,6 +19,8 @@ var server = http.createServer(function(req, res) {
 
 server.listen(3000);
 console.log("Server is running on port 3000.");
+
+
 //router
 function dispatch(req, res, url) {
   if (url == '/') {
@@ -23,9 +29,14 @@ function dispatch(req, res, url) {
   } else {
     var urls = url.split("/");
     if (urls[1] == "directories") {
-      var relaPath = url.replace("directories/", "");
-      var absPath = path.join(root, relaPath);
-      showDirectory(req, res, absPath);
+      if (urls[2] == "show") {
+        var relaPath = url.replace("directories/show", "");
+        showDirectory(req, res, relaPath);
+      } else if (urls[2] == "rename") {
+        var relaPath = url.replace("directories/rename", "");
+        renameDirectory(req, res, relaPath);
+      }
+    } else if (urls[1] == "files") {
     }
   }
 }
@@ -37,28 +48,55 @@ function index(req, res, indexPath) {
 };
 
 //directories/id
-function showDirectory(req, res, absPath) {
+function showDirectory(req, res, relaPath) {
+  try {
+    var result = listSubDirectories(relaPath);
+    var html = generateShowDirectoriesHtml(result);
+    res.writeHead(200, {"content-type": 'text/html'})
+    res.write(html);
+    res.end();
+  } catch (e) {
+    notFound(res);
+  }
 };
+
+function renameDirectory(req, res, oldPath, newPath) {
+  var absOldPath = path.join(root, oldPath);
+  var absNewPath = path.join(root, newPath);
+  fs.renameSync(absOldPath, absNewPath);
+}
 
 function showFile(req, res, absPath) {
 };
 
 //model
-function listSubDirectories(absPath) {
+function listSubDirectories(relaPath) {
+  var absPath = path.join(root, relaPath);
+  var itemList = fs.readdirSync(absPath);
+  var results = [];
+  for (var i in itemList) {
+    console.log(itemList[i]);
+    var filePath = path.join(absPath, itemList[i]);
+    var stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      results.push(
+          {"name": itemList[i],
+           "absPath": filePath,
+           "parentPath": absPath,
+           "relaPath": path.join(relaPath, itemList[i]),
+           "type": "directory"
+          });
+    } else {
+      results.push(
+          {"name": itemList[i],
+           "absPath": filePath,
+           "parentPath": absPath,
+           "relaPath": path.join(relaPath, itemList[i]),
+           "type": "file"
+          });
+    }}
+  return results;
 };
-
-//helper
-function badRequest(res) {
-  res.writeHead(400, {'Content-Type': 'text/plain'});
-  res.write('Error 400: Bad Request');
-  res.end();
-}
-
-function notFound(res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
-  res.write('Error 404: resource not found');
-  res.end();
-}
 
 function sendFile(res, filePath, fileContents) {
   res.writeHead(
@@ -83,64 +121,81 @@ function serveStatic(res, absPath) {
     }
   });
 };
-/*
-function writeFile() {
-  var url = parse(req.url);
-  var path = join(root, url.pathname);
-  var stream = fs.createReadStream(path);
-    stream.on('data', function(chunk){
-      res.write(chunk);
-    });
-    stream.on('end', function(){
-    res.end();
-  });
-}
-*/
-/*
-var cache = {};
 
-var server = http.createServer(function(request, response) {
-  var filePath = false;
-  if (request.url == '/') {
-    filePath = 'public/index.html';
-  } else {
-    filePath = 'public' + request.url;
-  }
-  var absPath = './' + filePath;
-  serveStatic(response, cache, absPath);
-});
-
-server.listen(3000, function() {
-  console.log("Server is listening on port 3000.");
-});
-
-function send404(response) {
-  response.writeHead(404, {'Content-Type': 'text/plain'});
-  response.write('Error 404: resource not found');
-  response.end();
+//helper
+function badRequest(res) {
+  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.write('Error 400: Bad Request');
+  res.end();
 }
 
-function sendFile(response, filePath, fileContents) {
-  response.writeHead(
+function notFound(res) {
+  res.writeHead(404, {'Content-Type': 'text/plain'});
+  res.write('Error 404: resource not found');
+  res.end();
+}
+
+//view
+function getTemplate(absPath){
+  var buf = fs.readFileSync(absPath, "utf8");
+  return buf.toString();
+}
+
+function generateShowDirectoriesHtml(itemList) {
+  var body = '<h1>Directory List</h1>'
+           + '<div id="wrapper-container">'
+           + '<table>'
+           + '<thead>'
+           + '<tr>'
+           + '<th>Name</th>'
+           + '<th>show</th>'
+           + '<th>rename</th>'
+           + '<th>move</th>'
+           + '<th>delete</th>'
+           + '</tr>'
+           + '</thead>'
+           + '<tbody>';
+           for (i in itemList) {
+             if (itemList[i]["type"] == "file") {
+               body     += '<tr class="file">'
+                        + '<td>' + itemList[i]["name"] + '</td>'
+                        + '<td>' + '<a href="/files/show' + itemList[i]["relaPath"] +'">' + 'show</a>' + '</td>'
+                        + '<td>' + '<a href="/files/rename' + itemList[i]["relaPath"] +'">' + 'rename</a>' + '</td>'
+                        + '<td>' + '<a href="/files/move' + itemList[i]["relaPath"] +'">' + 'move</a>' + '</td>'
+                        + '<td>' + '<a href="/files/delete' + itemList[i]["relaPath"] +'">' + 'delete</a>' + '</td>'
+                        + '</tr>';
+             } else {
+               body     += '<tr class="directory">'
+                        + '<td>' + itemList[i]["name"] + '</td>'
+                        + '<td>' + '<a href="/directories/show' + itemList[i]["relaPath"] +'">' + 'show</a>' + '</td>'
+                        + '<td>' + '<a href="/directories/rename' + itemList[i]["relaPath"] +'">' + 'rename</a>' + '</td>'
+                        + '<td>' + '<a href="/directories/move' + itemList[i]["relaPath"] +'">' + 'move</a>' + '</td>'
+                        + '<td>' + '<a href="/directories/delete' + itemList[i]["relaPath"] +'">' + 'delete</a>' + '</td>'
+                        + '</tr>';
+             }
+           }
+           body +=
+           + '</tbody>'
+           + '</table>'
+           + '</div>';
+  return htmlTemplate.replace('%body',body);
+}
+
+function generateHtmlRename() {
+  var body = '<div>'
+           + '<form method="post">'
+           + 'old name: <input type="text" name="oldname" placeholder="old name here" disabled><br>'
+           + 'new name: <input type="text" name="newname">'
+           + '<input type="submit" value="Submit">'
+           + '</form>';
+}
+
+/*
+function sendFile(res, filePath, fileContents) {
+  res.writeHead(
       200,
       {"content-type": mime.lookup(path.basename(filePath))}
       );
-  response.end(fileContents);
-}
-
-function serveStatic(response, absPath) {
-  fs.exists(absPath, function(exists) {
-    if (exists) {
-      fs.readFile(absPath, function(err, data) {
-        if (err) {
-          send404(response);
-        } else {
-          sendFile(response, absPath, data);
-        }
-      });
-    } else {
-      send404(response);
-    }
-  });
+  res.end(fileContents);
 }
 */
